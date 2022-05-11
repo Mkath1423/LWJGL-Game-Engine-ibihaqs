e327d08c4aa6ab24a4d830458aac0f2b504923ea
Sanad Alajrami <sanad@alajrami.com>
2022-05-11 10:54:03 -0400

    Added Planet constructor + updated Resource + Extractor
    Updated connection between Resource and ExtractorBuilding and added Planet constructor.

--- src/main/java/pocketplanets/Resource.java
+++ src/main/java/pocketplanets/Resource.java
@@ -5,26 +5,28 @@ import java.util.Random;
 public class Resource {
     private String resourceName;
     private int resourceId;
-    private int amount;
+    private int resourceAmount;
 
 
-    public Resource(String name, int id, int maxmimumAmount, int minimumAmount){
-        generateResource(name, id, maxmimumAmount, minimumAmount);
-    }
-
-    private void generateResource(String name, int id, int max, int min){
+    public Resource(String name, int id, int amount){
         resourceName = name;
         resourceId = id;
-
-        Random rand = new Random();
-        setAmount((max - min + 1) + min);
+        setAmount(amount);
     }
 
-    private void setAmount(int count){
-        amount = count;
+    public void setAmount(int amount){
+        resourceAmount = amount;
     }
 
     public int getAmount(){
-        return amount;
+        return resourceAmount;
+    }
+
+    public String getName(){
+        return resourceName;
+    }
+
+    public int getId(){
+        return resourceId;
     }
 }
