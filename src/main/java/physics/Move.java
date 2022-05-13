edfa3b6461e1987ebb944ec7b263c60d4a4afc96
CacctusMan <GWSBrooks-Prenger11-ICS2O1-01@hdsb.ca>
2022-05-13 10:16:00 -0400

    Modified Quad and addded circles

---
+++ src/main/java/physics/Move.java
@@ -0,0 +1,18 @@
+package physics;
+
+import components.Component;
+
+public class Move extends Component {
+ 
+    
+
+    // INITIALIZATION
+
+    // get transform from awake
+    // move will take velocity and acceleration
+    // time will be specified in update
+    // 
+
+
+
+}
